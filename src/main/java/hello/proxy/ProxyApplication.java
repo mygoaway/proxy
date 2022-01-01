581cf3cbcd26525107897d914e29fd071098187e
mygoaway2 <mygoaway2@gmail.com>
2022-01-01 10:59:16 +0900

    second commit

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Import;
 @Import(AppV1Config.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProxyApplication.class, args);
 	}
